ac5d2b4055530a43cfae21347b9affddd3308952
cn03390 <fengxiaoqian_bupt@126.com>
2019-07-29 15:01:13 +0800

    modify code3

--- src/main/java/application/DetectFace.java
+++ src/main/java/application/DetectFace.java
@@ -1,15 +1,69 @@
 package application;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import com.google.gson.Gson;
+import com.ibm.cloud.sdk.core.http.Response;
 import com.ibm.cloud.sdk.core.service.security.IamOptions;
 import com.ibm.watson.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
 import com.ibm.watson.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.visual_recognition.v3.model.DetectedFaces;
-
-public class DetectFace {
-	public static void main(String[] args) throws FileNotFoundException {
+ 
+/**
+ * Servlet implementation class Test1
+ */
+@WebServlet("/Test2")
+public class DetectFace extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+ 
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String url = request.getParameter("url");
+		System.out.println(name);
+		System.out.println(url);
+ 
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");// 注意設置為utf-8否則前端接收到的中文為亂碼
+		PrintWriter out = response.getWriter();
+ 
+		Gson gson = new Gson();
+		ArrayList<Test1Object> arraylistTest = new ArrayList<Test1Object>();
+ 
+		Test1Object t1 = new Test1Object();
+		Test1Object t2 = new Test1Object();
+ 
+		t1.setName(name);
+		t1.setUrl(url);
+		t2.setName(name);
+		t2.setUrl(url);
+ 
+		arraylistTest.add(t1);
+		arraylistTest.add(t2);
+ 
+		String info = gson.toJson(arraylistTest);
+		// 打印出檢查
+		System.out.println(info);
+		// 返回給前端
+//		out.write(info);
 		IamOptions options = new IamOptions.Builder()
 				  .apiKey("By62W_ehSwS2_zEXPdH2EvCQF_Hk6LaE_0JxkqOR34ht")
 				  .build();
@@ -21,6 +75,6 @@ public class DetectFace {
 				  .build();
 				DetectedFaces result = service.detectFaces(detectFacesOptions).execute().getResult();
 				System.out.println(result);
+				out.write(result.toString());
 	}
-
 }
--- target/MyAppSWLEJ-1.0-SNAPSHOT/js/data.js
+++ target/MyAppSWLEJ-1.0-SNAPSHOT/js/data.js
@@ -1,7 +1,7 @@
 var datajson = [
-    {"id":"001", "things":"食品","type":"大帐篷","locationone":116.409,"locationtwo":39.9097},
-    {"id":"002", "things":"担架","type":"大帐篷","locationone":116.419,"locationtwo":39.9187},
-    {"id":"003", "things":"包扎带","type":"小帐篷","locationone":116.429,"locationtwo":39.9157},
-    {"id":"004","things":"药品","type":"大帐篷","locationone":116.439,"locationtwo":39.9127},
-    {"id":"005","things":"担架","type":"小帐篷","locationone":116.449,"locationtwo":39.9177}
+    {"id":"001", "things":"Food","type":"BigTent","locationone":17.409,"locationtwo":48.9097},
+    {"id":"002", "things":"Stretcher","type":"BigTent","locationone":17.419,"locationtwo":48.9187},
+    {"id":"003", "things":"Bandage","type":"SmallTent","locationone":17.429,"locationtwo":48.9157},
+    {"id":"004","things":"Drugs","type":"BigTent","locationone":17.439,"locationtwo":48.9127},
+    {"id":"005","things":"Stretcher","type":"SmallTent","locationone":17.449,"locationtwo":48.9177}
 ]
